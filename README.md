5ee7073a2afa971e245c9badcf4e466c2933deed
William Chen <william101.chen@gmail.com>
2014-07-01 11:38:34 -0700

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 phytab_splitter
 ===============
 
-Splits Phytab files into FASTA
+This python library splits Phytab files into FASTA files into gene name - sequence tuples. The code is very rough, but should be usable.
+
+### More information
+
+[Phytab specification](http://osiris-phylogenetics.blogspot.com/2012/09/introduction-to-phytab-format.html)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'phytab_splitter',
+  packages = ['phytab_splitter'],
+  version = '0.01a',
+  description = 'Splits Phytab files into FASTA',
+  author = 'William Chen',
+  author_email = 'wtchensb@outlook.com',
+  url = 'https://github.com/wtchen/phytab_splitter',
+  download_url = 'https://github.com/wtchen/phytab_splitter/tarball/0.01a',
+  keywords = ['bioinformatics', 'phytab', 'fasta'],
+  classifiers = [],
+)
